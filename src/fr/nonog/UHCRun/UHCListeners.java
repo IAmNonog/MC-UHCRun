20fdebd22090e9c9c402e4714c02e9ad04adf522
IAmNonog <noahg250602@gmail.com>
2021-07-20 20:07:10 +0200

    First commit : init

---
+++ src/fr/nonog/UHCRun/UHCListeners.java
@@ -0,0 +1,36 @@
+package fr.nonog.UHCRun;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class UHCListeners implements Listener {
+
+    private UHCRun main;
+
+    public UHCListeners(UHCRun main) {
+        this.main = main;
+    }
+
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        if(!main.isGameLaunch()) {
+            player.teleport(new Location( (Bukkit.getWorld(main.getConfigur().getString("game.map"))) , main.getConfigur().getDouble("spawn.coordonate.x") , main.getConfigur().getDouble("spawn.coordonate.y") +3 , main.getConfigur().getDouble("spawn.coordonate.z") ));
+            player.getInventory().clear();
+            player.getInventory().setArmorContents(null);
+            player.setFoodLevel(20);
+            player.setGameMode(GameMode.ADVENTURE);
+            player.setHealth(20);
+            player.setLevel(0);
+
+
+        }
+    }
+}
---
+++ src/fr/nonog/UHCRun/UHCRun.java
@@ -0,0 +1,87 @@
+package fr.nonog.UHCRun;
+
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Vector;
+
+public class UHCRun extends JavaPlugin {
+
+    private FileConfiguration config;
+    private boolean gameLaunch;
+
+
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        config = this.getConfig();
+        gameLaunch = false;
+
+        String worldname = config.getString("game.map");
+        World world = Bukkit.getWorld(worldname);
+
+        WorldBorder wb = world.getWorldBorder();
+        wb.setCenter(config.getInt("spawn.coordonate.x"), config.getInt("spawn.coordonate.z"));
+        wb.setSize(config.getDouble("game.map-size"));
+
+
+        if(config.getBoolean("spawn.buildSpawn")) {
+            buildSpawn(world);
+        }
+
+
+
+        getServer().getPluginManager().registerEvents(new UHCListeners(this), this);
+
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[UHCRun] plugin enabled !");
+    }
+
+    @Override
+    public void onDisable() {
+
+
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[UHCRun] plugin disabled !");
+    }
+
+    public void buildSpawn(World w) {
+        Bukkit.broadcastMessage(ChatColor.GOLD + "[UHCRun] - Generate spawn...");
+        int xc = config.getInt("spawn.coordonate.x");
+        int yc = config.getInt("spawn.coordonate.y");
+        int zc = config.getInt("spawn.coordonate.z");
+
+        Vector<Block> spawn = new Vector<Block>();
+        for(int x=(xc-10); x<=(xc+10); x++) {
+            for(int z=(zc-10); z<=(zc+10); z++) {
+                spawn.add(w.getBlockAt(x, yc, z));
+                for(int y=yc; y<yc+6; y++) {
+                    spawn.add(w.getBlockAt(x, y, (zc+10)));
+                    spawn.add(w.getBlockAt(x, y, (zc-10)));
+                    spawn.add(w.getBlockAt((xc+10), y, z));
+                    spawn.add(w.getBlockAt((xc-10), y, z));
+                }
+            }
+
+        }
+
+        for(Block bl : spawn) {
+            bl.setType(Material.GLASS);
+        }
+        w.getBlockAt(xc, yc, zc).setType(Material.STAINED_GLASS);
+        Bukkit.broadcastMessage(ChatColor.GREEN + "[UHCRun] - Spawn generated");
+    }
+
+    public void setGameLaunch(boolean gameLaunch) {
+        this.gameLaunch = gameLaunch;
+    }
+
+    public boolean isGameLaunch() {
+        return gameLaunch;
+    }
+
+    public FileConfiguration getConfigur() {
+        return config;
+    }
+}
